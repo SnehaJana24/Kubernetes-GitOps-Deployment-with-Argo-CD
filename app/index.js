bb3f9b3c9d62f4f627a911fef645cd5e1621434e
Sneha Jana <snehajana461@gmail.com>
2026-08-18 22:43:13 +0530

    feat: update application message

--- app/index.js
+++ app/index.js
@@ -1,43 +1,51 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = process.env.PORT || 3000;
-const POD_NAME = process.env.POD_NAME || 'local';
+const POD_NAME = process.env.POD_NAME || "local";
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
 
-  if (req.url === '/health') {
+  if (req.url === "/health") {
     res.writeHead(200);
-    res.end(JSON.stringify({
-      status: 'healthy'
-    }));
+    res.end(
+      JSON.stringify({
+        status: "healthy",
+      }),
+    );
     return;
   }
 
-  if (req.url === '/info') {
+  if (req.url === "/info") {
     res.writeHead(200);
-    res.end(JSON.stringify({
-      application: 'Kubernetes GitOps Demo',
-      pod: POD_NAME,
-      message: 'Running inside Kubernetes'
-    }));
+    res.end(
+      JSON.stringify({
+        application: "Kubernetes GitOps Demo",
+        pod: POD_NAME,
+        message: "Running inside Kubernetes",
+      }),
+    );
     return;
   }
 
-  if (req.url === '/') {
+  if (req.url === "/") {
     res.writeHead(200);
-    res.end(JSON.stringify({
-      message: '🚀 Kubernetes GitOps Project',
-      status: 'running',
-      pod: POD_NAME
-    }));
+    res.end(
+      JSON.stringify({
+        message: "🚀 Kubernetes GitOps Project v2",
+        status: "running",
+        pod: POD_NAME,
+      }),
+    );
     return;
   }
 
   res.writeHead(404);
-  res.end(JSON.stringify({
-    error: 'Route not found'
-  }));
+  res.end(
+    JSON.stringify({
+      error: "Route not found",
+    }),
+  );
 });
 
 server.listen(PORT, () => {
